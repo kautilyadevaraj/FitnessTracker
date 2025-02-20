7baecca9da8d559f3609eada634db8f62770b282
Kautilya DK <kautilyadk@gmail.com>
2025-02-21 01:08:48 +0530

    added Discord OAuth

--- auth.ts
+++ auth.ts
@@ -1,10 +1,15 @@
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
 import LinkedIn from "next-auth/providers/linkedin";
+import Discord from "next-auth/providers/discord";
 import { FirestoreAdapter } from "@auth/firebase-adapter";
 import { firestore } from "@/lib/firestore";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [Google, LinkedIn],
+  providers: [
+    Google({ allowDangerousEmailAccountLinking: true }),
+    LinkedIn({ allowDangerousEmailAccountLinking: true }),
+    Discord({ allowDangerousEmailAccountLinking: true }),
+  ],
   adapter: FirestoreAdapter(firestore),
 });

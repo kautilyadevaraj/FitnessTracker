2b2433a49268f4738d3ce6899e742d9ae9f83dd6
Kautilya DK <kautilyadk@gmail.com>
2025-02-21 00:41:59 +0530

    Added LinkedIn OAuth Login

--- auth.ts
+++ auth.ts
@@ -1,9 +1,10 @@
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
+import LinkedIn from "next-auth/providers/linkedin";
 import { FirestoreAdapter } from "@auth/firebase-adapter";
 import { firestore } from "@/lib/firestore";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [Google],
+  providers: [Google, LinkedIn],
   adapter: FirestoreAdapter(firestore),
 });

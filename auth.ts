8e6fedad7b9aef1cddd44d972ebd3bb9bf26c992
Kautilya DK <kautilyadk@gmail.com>
2025-02-22 18:25:01 +0530

    added session management

--- auth.ts
+++ auth.ts
@@ -14,4 +14,18 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     GitHub({ allowDangerousEmailAccountLinking: true }),
   ],
   adapter: FirestoreAdapter(firestore),
+  pages: {
+    signIn: "/login",
+  },
+  debug: true,
+    //  By default, the `id` property does not exist on `token` or `session`. See the [TypeScript](https://authjs.dev/getting-started/typescript) on how to add it.
+  callbacks: {
+    jwt({ token, user }) {
+      if (user) { // User is available during sign-in
+        token.id = user.id
+      }
+      return token
+    },   
+  },
+
 });
